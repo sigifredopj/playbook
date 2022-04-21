16bbaea04abf001d2385813c65010d669d138f7e
sigifredopj <sigifredopj@gmail.com>
2022-04-20 23:20:51 -0500

    exampls

--- weekly_mission_2/examples/3-classesAndObjects/5-.js
+++ weekly_mission_2/examples/3-classesAndObjects/5-.js
@@ -1,3 +1,23 @@
 // Ejemplo 5: Atributos con valores por default
 
 
+class PullRequest {
+    constructor(repository, title, number){
+        this.repository = repository
+        this.title = title
+        this.number = number
+        this.status = "open"
+        this.dateCreated = new Date()
+    }
+    getInfo(){
+        return `This PR is in the repo: ${this.repository} (status: ${this.status}) and was created on ${this.dateCreated}`
+    }
+}
+console.log("Ejemplo 5: Atributos con valores por default")
+const myPR1 = new PullRequest("LaunchX", "Mi Primer PR", 100)
+console.log(myPR1.getInfo())
+
+    // Puedes instanciar n cantidad de objetos de la misma clase
+const myPR2 = new PullRequest("LaunchX", "Mi segundo PR", 99)
+console.log(myPR2.getInfo())
+
--- weekly_mission_2/examples/3-classesAndObjects/7-.js
+++ weekly_mission_2/examples/3-classesAndObjects/7-.js
@@ -1,4 +1,33 @@
 
 // Ejemplo 7: Setters para modificar los atributos del objeto
+class MissionCommander {
+    constructor(name, mission){
+        this.name = name
+        this.mission = mission
+        this.students = 40
+        this.lives = 6
+    }
+    
+    get getStudents(){
+        return this.students
+    }
+    
+    get getLives(){
+        return this.lives
+    }
+    
+    set setStudents(students){
+        this.students = students
+    }
+    
+    set setLives(lives){
+        this.lives = lives
+    }
+    }
+    
+    console.log("Ejemplo 7: Setters para modificar los atributos del objeto")   
+    const missionCommanderNode = new MissionCommander("Carlo", "NodeJS")
 
+    console.log(missionCommanderNode.getStudents) // 0 por default
+    console.log(missionCommanderNode.getLives)// 0 por default
 
